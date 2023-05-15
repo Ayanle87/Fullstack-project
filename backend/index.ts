1f799abab66fe0ca8c16d308ab370ca5c1037fd1
KevinITHS <kevin.ali@iths.se>
2023-05-15 14:03:57 +0200

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -6,10 +6,10 @@ const app = express();
 app.use(cors());
 
 app.get("/", (request, response) => {
-    console.log("hej");
-    response.send("Hello World!!!");
+  console.log("hej");
+  response.send("Hello World!kkkkkkkk!!");
 });
 
 app.listen(8080, () => {
-    console.log("Webbtjänsten kan nu ta emot anrop.");
+  console.log("Webbtjänsten kan nu ta emot anrop.");
 });

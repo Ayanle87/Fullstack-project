21deff2109d6c44ed9869096ae0fc193db619912
unknown <isabell.leoson@gmail.com>
2023-05-16 21:36:34 +0200

    Update Navbar.tsx
    Installed react-bootstrap

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+// import React from 'react';
+import { Button } from "react-bootstrap";
 
 const CustomNavbar: React.FC = () => {
-  return (
-    <div className="navbar">
-      <Button variant="link" className="text-decoration-none left-button">Logga in</Button>
-      <div className="right-buttons">
-        <Button variant="link" className="text-decoration-none button">Om</Button>
-        <Button variant="link" className="text-decoration-none button">Kontakt</Button>
-      </div>
-    </div>
-  );
-}
+    // return <h1>Navbar</h1>;
+    return (
+        <div className="navbar">
+            <Button variant="link" className="text-decoration-none left-button">
+                Logga in
+            </Button>
+            <div className="right-buttons">
+                <Button variant="link" className="text-decoration-none button">
+                    Om
+                </Button>
+                <Button variant="link" className="text-decoration-none button">
+                    Kontakt
+                </Button>
+            </div>
+        </div>
+    );
+};
 
 export default CustomNavbar;

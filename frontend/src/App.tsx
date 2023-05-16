b4eccb2009d18929a6f1813ab6c2bba40a242157
Ayanle87 <ayanle.ahmed87@gmail.com>
2023-05-16 15:00:02 +0200

    added a Snaazy map in landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<iframe
+title = "Snaazy Maps"
+style={{ width: "100%", height: "700px",border:"none"}}
+src="https://snazzymaps.com/embed/490724">
+   </iframe>
     </div>
   );
 }

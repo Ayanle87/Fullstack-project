78b1642900ce916db1c47e0e759d698b9474354b
KevinITHS <kevin.ali@iths.se>
2023-05-16 16:12:41 +0200

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-<iframe
-title = "Snaazy Maps"
-style={{ width: "100%", height: "700px",border:"none"}}
-src="https://snazzymaps.com/embed/490724">
-   </iframe>
+      <iframe
+        title="Snaazy Maps"
+        style={{ width: "100%", height: "700px", border: "none" }}
+        src="https://snazzymaps.com/embed/490724"
+      ></iframe>
+      <Navbar /> {/* Include Navbar component */}
     </div>
   );
 }

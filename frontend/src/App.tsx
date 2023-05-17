28fe2b6c11150054ed5edae7cd628f462bac0d31
Isabell <isabell.leoson@gmail.com>
2023-05-17 10:38:46 +0200

    added routes to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import './App.css';
-import Home from './Home';
-import './navbar.css';
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import "./navbar.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+import Root from "./Root";
+import ObjectCard from "./components/ObjectCard";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
+    const router = createHashRouter([
+        {
+            children: [
+                {
+                    element: <Home />,
+                    path: "/",
+                },
+                {
+                    element: <ObjectCard />,
+                    path: "/object",
+                },
+            ],
+            element: <Root />,
+        },
+    ]);
+    return (
+        <div className="App">
+            <Home />
+        </div>
+    );
+};
 
 export default App;
---
+++ frontend/src/views/TestView.tsx
@@ -0,0 +1,9 @@
+function TestView() {
+    return (
+        <>
+            <button>Objekt-knapp</button>
+        </>
+    );
+}
+
+export default TestView;

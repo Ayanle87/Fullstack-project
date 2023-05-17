f9787d5a474cfb399c8c313f4583cfa733afe5c2
Ayanle87 <ayanle.ahmed87@gmail.com>
2023-05-17 14:34:11 +0200

    added some packages

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -7,8 +7,8 @@ const Home: React.FC = () => {
       <div>
         <iframe
           title="Snaazy Maps"
-          style={{ width: "100%", height: "700px", border: "none" }}
-          src="https://snazzymaps.com/embed/490724"
+          style={{ width: "100%", height: "90vh", border: "none" }}
+          src={`https://snazzymaps.com/embed/490724?styles=[{"feature":"poi.business","stylers":[{"visibility":"off"}]}]&key="AIzaSyD4PHr_hX_LqK6x9AHG_heaXXrgKNIlDDk"`}
         ></iframe>
       </div>
       <div>

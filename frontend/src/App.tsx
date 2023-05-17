4ed2baf77bdd8bc7f640fc5f399a83d501b519ff
Isabell <isabell.leoson@gmail.com>
2023-05-17 13:06:46 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
     ]);
     return (
         <div className="App">
-            <TestView />
+            {/* <TestView /> */}
             <RouterProvider router={router} />
         </div>
     );

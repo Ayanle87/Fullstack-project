a8e1e2eeb9d58df4aa955e9511a2b8b2609b3901
Isabell <isabell.leoson@gmail.com>
2023-05-23 20:35:38 +0200

    added images, added more products to the database, added pins as buttons for the different categories

--- frontend/src/components/ObjectCard.tsx
+++ frontend/src/components/ObjectCard.tsx
@@ -9,7 +9,17 @@ interface Product {
     price: number;
     description: string;
     image: string;
+    category: string;
 }
+// src="/ux ikoner/Toggles50h/Logo100px.png"
+const categoryImages: { [key: string]: string } = {
+    Elektronik: "/ux ikoner/Pins/ElektronikMainD.png",
+    Fordon: "/ux ikoner/Pins/FordonMainD.png",
+    Fritid: "/ux ikoner/pins/FritidMain@0.png",
+    Hushåll: "/ux ikoner/pins/HusMain@0.png",
+    Kläder: "/ux ikoner/pins/KladerMain@0.png",
+    Övrigt: "/ux ikoner/pins/OvrigtMain@0.png",
+};
 
 const ObjectCard: React.FC = () => {
     const [result, setResult] = useState<Product[]>([]);
@@ -48,40 +58,65 @@ const ObjectCard: React.FC = () => {
             <StyledContainer>
                 {result.length > 0 &&
                     result.map((product) => (
-                        <button
+                        <img
+                            className="styledPins"
                             key={product.id}
-                            value={product.id}
+                            src={categoryImages[product.category]}
+                            alt={product.name}
                             onClick={() => handleClick(product.id)}
-                        >
-                            {product.name}
-                        </button>
+                        />
                     ))}
+                <StyledModal>
+                    <Modal
+                        isOpen={isModalOpen}
+                        onRequestClose={handleCloseModal}
+                        style={{
+                            overlay: {
+                                zIndex: 9999,
+                                width: "auto",
+                                height: "auto",
+                            },
+                        }}
+                    >
+                        {selectedProduct && (
+                            <ul>
+                                <li key={selectedProduct.id}>
+                                    <img
+                                        src="/ux ikoner/Pins/close-modal.png"
+                                        alt=""
+                                        style={closeStyle}
+                                        onClick={handleCloseModal}
+                                    />
+                                    <img
+                                        alt="product"
+                                        src={
+                                            "http://localhost:8080" +
+                                            selectedProduct.image
+                                        }
+                                        style={imgStyle}
+                                    />
 
-                <Modal isOpen={isModalOpen} onRequestClose={handleCloseModal}>
-                    <h1>Produkt</h1>
-
-                    {selectedProduct && (
-                        <ul>
-                            <li key={selectedProduct.id}>
-                                <img
-                                    alt="product"
-                                    src={
-                                        "http://localhost:8080" +
-                                        selectedProduct.image
-                                    }
-                                />
-
-                                <p>{selectedProduct.name}</p>
-                                <p>{selectedProduct.price}kr</p>
-                                <p>{selectedProduct.description}</p>
-                            </li>
-                        </ul>
-                    )}
-                    <button onClick={handleCloseModal}>Stäng</button>
-                </Modal>
+                                    <p>{selectedProduct.name}</p>
+                                    <p>{selectedProduct.price}kr</p>
+                                    <p>{selectedProduct.description}</p>
+                                </li>
+                            </ul>
+                        )}
+                    </Modal>
+                </StyledModal>
             </StyledContainer>
         </>
     );
+};
+
+const closeStyle = {
+    width: "5%",
+    height: "5%",
+};
+
+const imgStyle = {
+    width: "100%",
+    height: "100%",
 };
 
 const StyledContainer = styled.div`
@@ -89,6 +124,10 @@ const StyledContainer = styled.div`
 
     background-color: transparent;
     background: transparent;
+`;
+
+const StyledModal = styled.div`
+    position: absolute;
 `;
 
 export default ObjectCard;

f63366c2007c4cc4da8f73121c1cc973e8a639a3
Isabell <isabell.leoson@gmail.com>
2023-05-29 09:50:32 +0200

    update

--- frontend/src/components/ObjectCard.tsx
+++ frontend/src/components/ObjectCard.tsx
@@ -5,246 +5,237 @@ import styled from "styled-components";
 
 // Interface som specificerar vad som ska finnas i product
 interface Product {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-  image: string;
-  category: string;
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+    category: string;
 }
 // Kategori-ikonerna. Rubrikerna (Elektronik etc) matchar databasen och därför fattar koden vilken bild som tillhör vilken produkt
 
-interface ObjectCardProps {
-  onProductClick: (id: number) => void;
-  product:Product;
-}
-
-
 const categoryImages: { [key: string]: string } = {
-  Elektronik: "/ux ikoner/76h/ElectronicsPin76vh.png",
-  Fordon: "/ux ikoner/76h/VehiclePin76vh.png",
-  Fritid: "/ux ikoner/76h/SportPin76vh.png",
-  Hushåll: "/ux ikoner/76h/HomePin76vh.png",
-  Kläder: "/ux ikoner/76h/ClothesPin76vh.png",
-  Övrigt: "/ux ikoner/76h/OtherPin76vh.png",
+    Elektronik: "/ux ikoner/76h/ElectronicsPin76vh.png",
+    Fordon: "/ux ikoner/76h/VehiclePin76vh.png",
+    Fritid: "/ux ikoner/76h/SportPin76vh.png",
+    Hushåll: "/ux ikoner/76h/HomePin76vh.png",
+    Kläder: "/ux ikoner/76h/ClothesPin76vh.png",
+    Övrigt: "/ux ikoner/76h/OtherPin76vh.png",
 };
-
-
-
 
 // Om e pin blivit klickad på så byts den ut till en av dessa.
 const categoryImagesVisited: { [key: string]: string } = {
-  Elektronik: "/ux ikoner/76h/ElectronicsPinVisited76vh.png",
-  Fordon: "/ux ikoner/76h/VehiclePinVisited76vh.png",
-  Fritid: "/ux ikoner/76h/SportPinVisited76vh.png",
-  Hushåll: "/ux ikoner/76h/HomePinVisited76vh.png",
-  Kläder: "/ux ikoner/76h/ClothesPinVisited76vh.png",
-  Övrigt: "/ux ikoner/76h/OtherPinVisited76vh.png",
+    Elektronik: "/ux ikoner/76h/ElectronicsPinVisited76vh.png",
+    Fordon: "/ux ikoner/76h/VehiclePinVisited76vh.png",
+    Fritid: "/ux ikoner/76h/SportPinVisited76vh.png",
+    Hushåll: "/ux ikoner/76h/HomePinVisited76vh.png",
+    Kläder: "/ux ikoner/76h/ClothesPinVisited76vh.png",
+    Övrigt: "/ux ikoner/76h/OtherPinVisited76vh.png",
 };
 
 // Huvudfunktionen
-const ObjectCard:React.FC<ObjectCardProps> = ({ onProductClick,product })=> {
-  // Här sparas det som fecthas från backend
-  const [result, setResult] = useState<Product[]>([]);
+const ObjectCard: React.FC = () => {
+    // Här sparas det som fecthas från backend
+    const [result, setResult] = useState<Product[]>([]);
 
-  // Sparar ID:t på vald produkt. Det gör att rätt produkt visas när den är vald
-  const [selectedProductId, setSelectedProductId] = useState<number | null>(
-    null
-  );
+    // Sparar ID:t på vald produkt. Det gör att rätt produkt visas när den är vald
+    const [selectedProductId, setSelectedProductId] = useState<number | null>(
+        null
+    );
 
-  //För att kunna öppna och stänga modalen
-  const [isModalOpen, setModalOpen] = useState(false);
+    //För att kunna öppna och stänga modalen
+    const [isModalOpen, setModalOpen] = useState(false);
 
+    // Det som skrivs in i sökfältet
+    const [searchValue, setSearchValue] = useState("");
 
-    // const handleClick = (id: number) => {
-    //     setSelectedProductId(id);
-    //     handleOpenModal();
-    // };
+    // Om en pin blivit klickad på så ska den byta bild
+    const [visitedPins, setVisitedPins] = useState<number[]>([]);
 
-    const handleClick = (id: number) => {
-    onProductClick(id);
-    handleOpenModal();
-  };
+    // Hämtar datan från backendet
+    useEffect(() => {
+        Modal.setAppElement("#root");
 
-  // Det som skrivs in i sökfältet
-  const [searchValue, setSearchValue] = useState("");
+        axios
+            .get("http://localhost:8080/")
+            .then((response) => {
+                setResult(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
-  // Om en pin blivit klickad på så ska den byta bild
-  const [visitedPins, setVisitedPins] = useState<number[]>([]);
+    // Söker igenom det som fetchats och och letar upp det matchde id:numret.
+    const selectedProduct = selectedProductId
+        ? result.find((product) => product.id === selectedProductId)
+        : null;
 
-  // Hämtar datan från backendet
-  useEffect(() => {
-    Modal.setAppElement("#root");
+    // Funktion som sätter selectedProductIds useState till det ID som klickats på, samt uppdaterar pinsen till nya om de blivit klickade på och öppnar modalen.
+    const handleClick = (id: number, category: string) => {
+        setSelectedProductId(id);
+        handleOpenModal();
 
-    axios
-      .get("http://localhost:8080/")
-      .then((response) => {
-        setResult(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+        setVisitedPins((prevVisitedPins) => [...prevVisitedPins, id]);
+    };
 
-  // Söker igenom det som fetchats och och letar upp det matchde id:numret.
-  const selectedProduct = selectedProductId
-    ? result.find((product) => product.id === selectedProductId)
-    : null;
+    // Öppnar modalen när man klickar på en pin
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
 
-  // Funktion som sätter selectedProductIds useState till det ID som klickats på, samt uppdaterar pinsen till nya om de blivit klickade på och öppnar modalen.
-  const handleClick = (id: number, category: string) => {
-    setSelectedProductId(id);
-    handleOpenModal();
+    // Stänger modalen när man klickar på en pin
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
 
-    setVisitedPins((prevVisitedPins) => [...prevVisitedPins, id]);
-  };
+    return (
+        <>
+            <div>
+                {/* Sökfält. searchValue är det som skrivs in.  */}
+                <form>
+                    <input
+                        type="text"
+                        value={searchValue}
+                        onChange={(event) => setSearchValue(event.target.value)}
+                    />
+                </form>
+            </div>
 
-  // Öppnar modalen när man klickar på en pin
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
+            {result.length > 0 &&
+                result
 
-  // Stänger modalen när man klickar på en pin
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
+                    // Filtrerar det som skrivs i sökfältet (searchValue) och jämnför med productkategorierna och produktnamnen. Funkar att söka med både liten och stor bokstav pga toLowerCase.
+                    .filter(
+                        (product) =>
+                            product.category
+                                .toLowerCase()
+                                .includes(searchValue.toLowerCase()) ||
+                            product.name
+                                .toLowerCase()
+                                .includes(searchValue.toLowerCase())
+                    )
 
-  return (
-    <>
-      <div>
-        {/* Sökfält. searchValue är det som skrivs in.  */}
-        <form>
-          <input
-            type="text"
-            value={searchValue}
-            onChange={(event) => setSearchValue(event.target.value)}
-          />
-        </form>
-      </div>
+                    //skapar en array av img baserat på resultatet från fetchen. I src sätter kategorin rätt kategori-bild uifrån produktkatergorin från databasen.Om en pin blivit klickad byts bild (Se categoryImages och categoryImagesVisited längst upp). HandleClick skickar med produkt id:t som argument för att rätt produkt ska visas.
+                    .map((product) => (
+                        <img
+                            className="styledPins"
+                            key={product.id}
+                            src={
+                                visitedPins.includes(product.id)
+                                    ? categoryImagesVisited[product.category]
+                                    : categoryImages[product.category]
+                            }
+                            alt={product.name}
+                            onClick={() =>
+                                handleClick(product.id, product.category)
+                            }
+                        />
+                    ))}
+            {/* Modalen som öppnas 'onClick' på pinsen. Modalen innehåller namn, bild, pris, produktbeskrivning. */}
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={handleCloseModal}
+                className="smallModalclass"
+                style={{
+                    overlay: {
+                        zIndex: 9999,
+                    },
+                }}
+            >
+                {selectedProduct && (
+                    <StyledContainer>
+                        <Ul>
+                            <Li key={selectedProduct.id}>
+                                <StyledImgDiv>
+                                    <img
+                                        src="/ux ikoner/Pins/close-modal.png"
+                                        alt=""
+                                        className="closeStyle"
+                                        style={closeStyle}
+                                        onClick={handleCloseModal}
+                                    />
 
-      {result.length > 0 &&
-        result
+                                    <img
+                                        alt="product"
+                                        src={
+                                            "http://localhost:8080" +
+                                            selectedProduct.image
+                                        }
+                                        style={imgStyle}
+                                        className="imgStyle"
+                                    />
+                                </StyledImgDiv>
 
-          // Filtrerar det som skrivs i sökfältet (searchValue) och jämnför med productkategorierna och produktnamnen. Funkar att söka med både liten och stor bokstav pga toLowerCase.
-          .filter(
-            (product) =>
-              product.category
-                .toLowerCase()
-                .includes(searchValue.toLowerCase()) ||
-              product.name.toLowerCase().includes(searchValue.toLowerCase())
-          )
+                                <StyledTopContainer>
+                                    <StyledH1>{selectedProduct.name}</StyledH1>
 
-          //skapar en array av img baserat på resultatet från fetchen. I src sätter kategorin rätt kategori-bild uifrån produktkatergorin från databasen.Om en pin blivit klickad byts bild (Se categoryImages och categoryImagesVisited längst upp). HandleClick skickar med produkt id:t som argument för att rätt produkt ska visas.
-          .map((product) => (
-            <img
-              className="styledPins"
-              key={product.id}
-              src={
-                visitedPins.includes(product.id)
-                  ? categoryImagesVisited[product.category]
-                  : categoryImages[product.category]
-              }
-              alt={product.name}
-              onClick={() => handleClick(product.id, product.category)}
-            />
-          ))}
-      {/* Modalen som öppnas 'onClick' på pinsen. Modalen innehåller namn, bild, pris, produktbeskrivning. */}
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={handleCloseModal}
-        className="modalclass"
-        style={{
-          overlay: {
-            zIndex: 9999,
-          },
-        }}
-      >
-        {selectedProduct && (
-          <StyledContainer>
-            <Ul>
-              <Li key={selectedProduct.id}>
-                <StyledImgDiv>
-                  <img
-                    src="/ux ikoner/Pins/close-modal.png"
-                    alt=""
-                    className="closeStyle"
-                    style={closeStyle}
-                    onClick={handleCloseModal}
-                  />
+                                    <StyledPriceDistanceContainer>
+                                        <StyledPrice>
+                                            {selectedProduct.price}kr
+                                        </StyledPrice>
+                                        <StyledDistance>
+                                            500m bort
+                                        </StyledDistance>
+                                    </StyledPriceDistanceContainer>
+                                </StyledTopContainer>
 
-                  <img
-                    alt="product"
-                    src={"http://localhost:8080" + selectedProduct.image}
-                    style={imgStyle}
-                    className="imgStyle"
-                  />
-                </StyledImgDiv>
-
-                <StyledTopContainer>
-                  <StyledH1>{selectedProduct.name}</StyledH1>
-
-                  <StyledPriceDistanceContainer>
-                    <StyledPrice>{selectedProduct.price}kr</StyledPrice>
-                    <StyledDistance>500m bort</StyledDistance>
-                  </StyledPriceDistanceContainer>
-                </StyledTopContainer>
-
-                <StyledDescriptionDiv>
-                  <StyledDescription>
-                    {selectedProduct.description}
-                  </StyledDescription>
-                </StyledDescriptionDiv>
-              </Li>
-            </Ul>
-          </StyledContainer>
-        )}
-      </Modal>
-    </>
-  );
+                                <StyledDescriptionDiv>
+                                    <StyledDescription>
+                                        {selectedProduct.description}
+                                    </StyledDescription>
+                                </StyledDescriptionDiv>
+                            </Li>
+                        </Ul>
+                    </StyledContainer>
+                )}
+            </Modal>
+        </>
+    );
 };
 
 // CSS
 
 const closeStyle = {
-  width: "17.74px",
-  height: "17.74px",
-  top: "5px",
-  right: "10px",
-  zIndex: "1",
+    width: "17.74px",
+    height: "17.74px",
+    top: "5px",
+    right: "10px",
+    zIndex: "1",
 };
 
 const imgStyle = {
-  width: "100%",
-  height: "100%",
-  right: "0",
-  zIndex: "0",
+    width: "100%",
+    height: "100%",
+    right: "0",
+    zIndex: "0",
 };
 
 const StyledContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const StyledImgDiv = styled.div`
-  width: 100%;
-  // width: 374px;
+    width: 100%;
+    // width: 374px;
 
-  height: 309.84px;
-  position: relative;
-  background: #495057;
-  object-fit: contain;
+    height: 309.84px;
+    position: relative;
+    background: #495057;
+    object-fit: contain;
 
-  z-index: 0;
+    z-index: 0;
 `;
 
 const StyledTopContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const StyledH1 = styled.h1`
-
+   
     font-family: "Open Sans", bold, sans-serif;
     font-style: normal;
     font-weight: 600;
@@ -259,83 +250,83 @@ const StyledH1 = styled.h1`
 `;
 
 const StyledPriceDistanceContainer = styled.div`
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  margin-left: 15px;
-  margin-right: 15px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    margin-left: 15px;
+    margin-right: 15px;
 `;
 
 const StyledPrice = styled.p`
-  font-family: "Open Sans", bold, sans-serif;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 16px;
-  line-height: 22px;
-  letter-spacing: 0.25px;
+    font-family: "Open Sans", bold, sans-serif;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    letter-spacing: 0.25px;
 
-  // display: flex;
-  // align-items: center;
-  // text-align: right;
+    // display: flex;
+    // align-items: center;
+    // text-align: right;
 `;
 const StyledDistance = styled.p`
-  font-family: "Open Sans", sans-serif;
+    font-family: "Open Sans", sans-serif;
 
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 12px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 12px;
 
-  letter-spacing: 0.135894px;
+    letter-spacing: 0.135894px;
 
-  color: #000000;
+    color: #000000;
 `;
 
 const StyledDescription = styled.p`
-  position: relative;
+    position: relative;
 
-  font-family: "Open Sans", sans-serif;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 19px;
-  letter-spacing: 0.25px;
+    font-family: "Open Sans", sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 19px;
+    letter-spacing: 0.25px;
 
-  //     display: flex;
-  // align-items: center;
+    //     display: flex;
+    // align-items: center;
 `;
 
 const StyledDescriptionDiv = styled.div`
-  box-sizing: border-box;
-  display: flex;
-  // object-fit: contain;
-  width: 322.15px;
-  height: 265.18px;
-  padding: 10px;
-  // align-items: center;
-  margin-left: 15px;
-  margin-top: 10px;
+    box-sizing: border-box;
+    display: flex;
+    // object-fit: contain;
+    width: 322.15px;
+    height: 265.18px;
+    padding: 10px;
+    // align-items: center;
+    margin-left: 15px;
+    margin-top: 10px;
 
-  border: 0.543575px solid #c0d0b9;
-  border-radius: 2.71787px;
+    border: 0.543575px solid #c0d0b9;
+    border-radius: 2.71787px;
 `;
 
 const Ul = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0px;
-  gap: 75.62px;
-  isolation: isolate;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+    gap: 75.62px;
+    isolation: isolate;
 
-  width: 100%;
-  height: 733.38px;
-  left: 13px;
-  top: 50.57px;
+    width: 100%;
+    height: 733.38px;
+    left: 13px;
+    top: 50.57px;
 `;
 
 const Li = styled.li`
-  list-style: none;
+    list-style: none;
 `;
 
 export default ObjectCard;

fcb6c4e6bb38e9babd2d69ddb71006973398397f
Ayanle87 <ayanle.ahmed87@gmail.com>
2023-06-07 15:18:06 +0200

    Flytta GET / till GET /products

--- backend/index.ts
+++ backend/index.ts
@@ -20,7 +20,7 @@ client.connect();
 const app = express();
 app.use(cors());
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get("/", async (request, response) => {
+app.get("/products", async (request, response) => {
     try {
         console.log(request.params);
         console.log(request);

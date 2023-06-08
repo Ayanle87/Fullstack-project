2e2a8194e056f41bbf7b747e67383e63154f5c0f
Isabell <isabell.leoson@gmail.com>
2023-06-08 12:10:42 +0200

    update

--- frontend/src/MobileNavbar.tsx
+++ frontend/src/MobileNavbar.tsx
@@ -4,43 +4,37 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import "./MobileNavbar.css";
 // FaBars
 function MobileNavbar(): JSX.Element {
-	const navRef = useRef<HTMLDivElement>(null);
+    const navRef = useRef<HTMLDivElement>(null);
 
-	const showNavbar = () => {
-		if (navRef.current) {
-			navRef.current.classList.toggle(
-				"responsive_nav"
-			);
-		}
-	};
+    const showNavbar = () => {
+        if (navRef.current) {
+            navRef.current.classList.toggle("responsive_nav");
+        }
+    };
 
+    return (
+        <header>
+            <div>
+                <img
+                    src="/ux ikoner/Toggles50h/Logo100px.png"
+                    alt="Logo"
+                    className="logo-image"
+                />
+            </div>
+            <nav ref={navRef}>
+                <a href="/#">Logga in</a>
+                <a href="/#">Om</a>
+                <a href="/#">Kontakt</a>
 
-	return (
-		<header>
-			<div>
-				<img
-					src="/ux ikoner/Toggles50h/Logo100px.png"
-					alt="Logo"
-					className="logo-image"
-				/>
-			</div>
-			<nav ref={navRef}>
-				<a href="/#">Logga in</a>
-				<a href="/#">Om</a>
-				<a href="/#">Kontakt</a>
-
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}
-				>
-					<FaTimes />
-				</button>
-			</nav>
-			<button className="nav-btn" onClick={showNavbar}>
-				< FaBars/>
-			</button>
-		</header>
-	);
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+        </header>
+    );
 }
 
 export default MobileNavbar;
